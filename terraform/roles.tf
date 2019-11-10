9134aceb3cdec284c95f5ae6cf6829ab7f06313c
Chris Scott-Thomas <chris.scott-Thomas@sky.uk>
2019-11-10 17:57:37 +0000

    This is making me weep

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -2,7 +2,6 @@
 
 # ECS task role data
 data "aws_iam_policy_document" "ecs_task_execution_role" {
-  version = "2012-10-17"
   statement {
     sid = ""
     effect = "Allow"
@@ -17,14 +16,13 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 
 # ECS Task role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = var.ecs_task_execution_role_name
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+  name = "${var.ecs_task_execution_role_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_task_execution_role.json}"
 }
 
-# ECS task role policy
+# ECS task role policy attachment
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  role = aws_iam_role.ecs_task_execution_role.name
+  role       = "${aws_iam_role.ecs_task_execution_role.name}"
   policy_arn = "arn:aws:iam:aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
