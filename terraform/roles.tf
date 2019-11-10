b396709a8a030fb97b0648ce4e3041e36f2ddf3c
Chris Scott-Thomas <chris.scott-Thomas@sky.uk>
2019-11-10 11:26:50 +0000

    Create IAM ECS Roles

---
+++ terraform/roles.tf
@@ -0,0 +1,30 @@
+# roles.tf
+
+# ECS task role data
+data "aws_iam_policy_document" "ecs_task_execution_role" {
+  version = "2019-11-10"
+  statement {
+    sid = ""
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+  
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+# ECS Task role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = var.ecs_task_execution_role_name
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
+
+# ECS task role policy
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam:aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
