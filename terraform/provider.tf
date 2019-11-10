69b1d2732919a9c8d43223a5ff1abd1f45c15749
Chris Scott-Thomas <chris.scott-Thomas@sky.uk>
2019-11-10 11:01:37 +0000

    Fixed context

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@
 # Specify the provider and access details
 
 provider "aws" {
-  shared_credential_file = "$HOME/.aws/credentials" #default for Mac/Linux
+  shared_credentials_file = "$HOME/.aws/credentials" #default for Mac/Linux
   profile                = "default"
   region                 = "var.aws_region"         #variable ref for region, adds flexability - set in variables.tf
 }
