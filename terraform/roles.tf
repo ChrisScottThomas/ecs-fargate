888a94812b988c83296a1a6ed45c7e3f6ba342f3
Chris Scott-Thomas <chris.scott-Thomas@sky.uk>
2019-11-11 07:44:58 +0000

    Fixed roles, main issue was a missing :

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -3,8 +3,6 @@
 # ECS task role data
 data "aws_iam_policy_document" "ecs_task_execution_role" {
   statement {
-    sid = ""
-    effect = "Allow"
     actions = ["sts:AssumeRole"]
   
     principals {
@@ -16,13 +14,22 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 
 # ECS Task role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "${var.ecs_task_execution_role_name}"
+  name               = "${var.ecs_task_execution_role_name}"
   assume_role_policy = "${data.aws_iam_policy_document.ecs_task_execution_role.json}"
+}
+
+data "aws_iam_policy" "ecs_task_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_policy" "ecs_task_execution_policy" {
+  name   = "${aws_iam_role.ecs_task_execution_role.name}"
+  policy = "${data.aws_iam_policy.ecs_task_execution.policy}"
 }
 
 # ECS task role policy attachment
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = "${aws_iam_role.ecs_task_execution_role.name}"
-  policy_arn = "arn:aws:iam:aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "${aws_iam_policy.ecs_task_execution_policy.arn}"
 }
