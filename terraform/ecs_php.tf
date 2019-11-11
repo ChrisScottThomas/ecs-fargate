8a8e66ccd01ef37356dc6d81b5fc0ff76e948a59
Chris Scott-Thomas <chris.scott-Thomas@sky.uk>
2019-11-11 07:45:18 +0000

    Added network config

--- terraform/ecs_php.tf
+++ terraform/ecs_php.tf
@@ -27,5 +27,11 @@ resource "aws_ecs_service" "php" {
   desired_count   = var.app_count
   launch_type     = "FARGATE"
 
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = aws_subnet.private.*.id
+    assign_public_ip = false
+  }
+
   depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_role]
 }
